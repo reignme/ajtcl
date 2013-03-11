334787de966557749d29ccc29a32fafffb2e24e2
Eric Rongo <erongo@quicinc.com>
2013-03-11 09:22:22 -0700

    new samples

---
+++ host/arduino/examples/AJ_svclite/AJ_svclite.ino
@@ -0,0 +1,79 @@
+/**
+ * @file
+ */
+/******************************************************************************
+ * Copyright 2013, Qualcomm Innovation Center, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ ******************************************************************************/
+
+#include <stdint.h>
+#include <stdarg.h>
+//#include <stdio.h>
+
+#include <alljoyn.h>
+
+#include <SPI.h>
+#ifdef WIFI_UDP_WORKING
+#include <WiFi.h>
+#else
+#include <Ethernet.h>
+#endif
+
+int AJ_Main(void);
+
+void setup() {
+    //Initialize serial and wait for port to open:
+    Serial.begin(9600);
+    while (!Serial) {
+        ; // wait for serial port to connect. Needed for Leonardo only
+    }
+    
+    printf("setup");
+    
+#ifdef WIFI_UDP_WORKING
+    char ssid[] = "eric-wifi";
+    int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+    if (WiFi.status() == WL_NO_SHIELD) {
+        Serial.println("WiFi shield not present");
+        // don't continue:
+        while (true) ;
+    }
+
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to open SSID: ");
+        Serial.println(ssid);
+        WiFi.begin(ssid);
+
+        // wait 10 seconds for connection:
+        delay(10000);
+    }
+#else
+    byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0xA7, 0xCA };
+    // start the Ethernet connection:
+    if (Ethernet.begin(mac) == 0) {
+        printf("Failed to configure Ethernet using DHCP\n");
+        // no point in carrying on, so do nothing forevermore:
+        for (;;)
+            ;
+    }
+#endif
+
+}
+
+
+void loop() {
+    AJ_Main();
+}
+
