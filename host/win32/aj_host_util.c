0593139b1c04a7687d84d9f40c5cdc9cf43d7a55
gburns <gburns@quicinc.com>
2013-03-13 12:20:27 -0700

    Implemted platform independent pool based malloc

--- host/win32/aj_host_util.c
+++ host/win32/aj_host_util.c
@@ -20,6 +20,7 @@
 #include <windows.h>
 #include <sys/timeb.h>
 #include <stdio.h>
+#include <assert.h>
 
 #include "aj_host.h"
 #include "aj_util.h"
@@ -49,46 +50,109 @@ uint32_t AJ_GetElapsedTime(AJ_Time* timer, uint8_t cumulative)
     return elapsed;
 }
 
-/*
- * Simulate the kind of pool-based allocation implemented on an RTOS
- */
+#define MIN_BLOCK_SIZE 16
+
+typedef struct _MemBlock {
+    struct _MemBlock* next;
+    uint8_t mem[MIN_BLOCK_SIZE - sizeof(void*)];
+} MemBlock;
+
 typedef struct _MemPool {
-    size_t size;
-    size_t free;
+    const uint16_t size;     /* Size of the pool entries in bytes */
+    const uint16_t entries;  /* Number of entries in this pool */
+    void* endOfPool;         /* Address of end of this pool */
+    MemBlock* freeList;      /* Linked free list for this pool */
 } MemPool;
 
 static MemPool memPools[] = {
-    { 32,   1  },
-    { 96,   4, },
-    { 192,  1, }
+    { 32,   1,  NULL, NULL },
+    { 96,   4,  NULL, NULL },
+    { 192,  1,  NULL, NULL }
 };
+
+#define HEAP_SIZE 720
+
+static uint32_t heap[HEAP_SIZE / 4];
+
+
+static void InitPools()
+{
+#ifndef NDEBUG
+    size_t totalSz = 0;
+#endif
+    size_t i;
+    size_t n;
+    uint8_t* heapPtr = (uint8_t*)heap;
+
+    for (i = 0; i < ArraySize(memPools); ++i) {
+        /*
+         * Add all blocks to the pool free list
+         */
+        for (n = memPools[i].entries; n != 0; --n) {
+            MemBlock* block = (MemBlock*)heapPtr;
+            block->next = memPools[i].freeList;
+            memPools[i].freeList = block;
+            heapPtr += memPools[i].size;
+#ifndef NDEBUG
+            totalSz += memPools[i].size;
+            assert(totalSz <= sizeof(heap));
+#endif
+        }
+        /*
+         * Save end of pool pointer for use by AJ_Free
+         */
+        memPools[i].endOfPool = (void*)heapPtr;
+    }
+}
 
 void* AJ_Malloc(size_t sz)
 {
-    size_t* mem;
     size_t i;
+
+    /*
+     * One time initialization
+     */
+    if (!memPools[0].endOfPool) {
+        InitPools();
+    }
+    /*
+     * Find smallest pool that can satisfy the allocation
+     */
     for (i = 0; i < ArraySize(memPools); ++i) {
-        if ((sz <= memPools[i].size) && (memPools[i].free > 0)) {
+        if ((sz <= memPools[i].size) && memPools[i].freeList) {
+            MemBlock* block = memPools[i].freeList;
             //printf("AJ_Malloc pool %d allocated %d\n", memPools[i].size, sz);
-            --memPools[i].free;
-            mem = (size_t*)malloc(sizeof(size_t) + sz);
-            mem[0] = i;
-            return &mem[1];
+            memPools[i].freeList = block->next;
+            return (void*)block;
         }
     }
+#ifndef NDEBUG
     printf("AJ_Malloc of %d bytes failed\n", sz);
     for (i = 0; i < ArraySize(memPools); ++i) {
-        printf("    Pool %d free %d\n", memPools[i].size, memPools[i].free);
+        printf("    Pool %d %s\n", memPools[i].size, memPools[i].freeList ? "available" : "depleted");
     }
+#endif
     return NULL;
 }
 
 void AJ_Free(void* mem)
 {
+    size_t i;
+
     if (mem) {
-        size_t* m = (size_t*)mem - 1;
-        //printf("AJ_Free pool %d\n", memPools[*m].size);
-        ++memPools[*m].free;
-        free(m);
+        assert((ptrdiff_t)mem >= (ptrdiff_t)heap);
+        /*
+         * Locate the pool from which the released memory was allocated
+         */
+        for (i = 0; i < ArraySize(memPools); ++i) {
+            if ((ptrdiff_t)mem < (ptrdiff_t)memPools[i].endOfPool) {
+                MemBlock* block = (MemBlock*)mem;
+                block->next = memPools[i].freeList;
+                memPools[i].freeList = block;
+                //printf("AJ_Free pool %d\n", memPools[i].size);
+                break;
+            }
+        }
+        assert(i < ArraySize(memPools));
     }
 }
