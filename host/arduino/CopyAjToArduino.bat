fb2b9a179d5eb617e0fdc80b9042a3c5b4dfd49b
Chris Pezzee <cpezzee@quicinc.com>
2013-03-14 15:53:30 -0700

    match the script to the scons build output

--- host/arduino/CopyAjToArduino.bat
+++ host/arduino/CopyAjToArduino.bat
@@ -3,10 +3,5 @@ SETLOCAL
 SET SRC=c:\cygwin\home\%USERNAME%\repos\alljoyn-tc\ajtcl
 SET DST=c:\arduino\arduino-1.5.2\hardware\arduino\sam\libraries\AllJoyn
 
-for %%I in (%DST%\*.cpp) do del %%I
-
-xcopy /Y /D /S /I %SRC%\inc\* %DST%
-xcopy /Y /D /S /I %SRC%\src\* %DST%
-xcopy /Y /D /S /I %SRC%\host\arduino\* %DST%
-
-for %%I in (%DST%\*.c) do ren %%I %%~nI.cpp
+rmdir /S /Q %DST%
+xcopy /Y /D /S /I %SRC%\build\arduino_due\libraries\AllJoyn %DST%
