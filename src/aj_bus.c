reignme/ajtcl

commit e69419ec6cec98ca6bcb89347d62e1cfd55de134

AllJoyn-2268: Clear message after consuming it so that only reply is sent. Added checks to handle cleared message structure. While in the code took opportunity to map some AJ_Printf() calls to use the new debug APIs.

src/aj_bus.c
@@ -336,6 +336,11 @@ AJ_Status AJ_BusHandleBusMessage(AJ_Message* msg)
     char* newOwner;
     AJ_Message reply;
 
+    /*
+     * Set hdr to NULL so we can detect below if we have a reply to send.
+     */
+    reply.hdr = NULL;
+
     switch (msg->msgId) {
     case AJ_METHOD_PING:
         status = AJ_MarshalReplyMsg(msg, &reply);
@@ -409,12 +414,12 @@ AJ_Status AJ_BusHandleBusMessage(AJ_Message* msg)
         break;
 
     default:
-        if (msg->hdr->msgType == AJ_MSG_METHOD_CALL) {
+        if (msg->hdr && (msg->hdr->msgType == AJ_MSG_METHOD_CALL)) {
             status = AJ_MarshalErrorMsg(msg, &reply, AJ_ErrRejected);
         }
         break;
     }
-    if ((status == AJ_OK) && (msg->hdr->msgType == AJ_MSG_METHOD_CALL)) {
+    if ((status == AJ_OK) && reply.hdr) {
         status = AJ_DeliverMsg(&reply);
     }
     return status;

src/aj_introspect.c
@@ -21,6 +21,7 @@
 #include "aj_introspect.h"
 #include "aj_std.h"
 #include "aj_msg.h"
+#include "aj_debug.h"
 #include "aj_util.h"
 
 /*
@@ -299,7 +300,7 @@ void AJ_PrintXML(const AJ_Object* obj)
     emptyList.interfaces = NULL;
     status = GenXML(PrintXML, NULL, obj, &emptyList);
     if (status != AJ_OK) {
-        AJ_Printf("\nFailed to generate XML - check interface descriptions for errors\n");
+        AJ_ErrPrintf(("\nFailed to generate XML - check interface descriptions for errors\n"));
     }
 }
 #endif
@@ -380,13 +381,13 @@ AJ_Status AJ_HandleIntrospectRequest(const AJ_Message* msg, AJ_Message* reply)
         context.len = 0;
         status = GenXML(SizeXML, &context.len, obj, objectLists[1]);
         if (status != AJ_OK) {
-            AJ_Printf("Failed to generate XML - check interface descriptions for errors\n");
+            AJ_ErrPrintf(("Failed to generate XML - check interface descriptions for errors\n"));
             return status;
         }
         /*
          * Second pass marshals the XML
          */
-        AJ_Printf("AJ_HandleIntrospectRequest() %d bytes of XML\n", context.len);
+        AJ_InfoPrintf(("AJ_HandleIntrospectRequest() %d bytes of XML\n", context.len));
         AJ_MarshalReplyMsg(msg, reply);
         /*
          * Do a partial delivery
@@ -415,7 +416,7 @@ AJ_Status AJ_HandleIntrospectRequest(const AJ_Message* msg, AJ_Message* reply)
         /*
          * Return a ServiceUnknown error response
          */
-        AJ_Printf("AJ_HandleIntrospectRequest() NO MATCH for %s\n", msg->objPath);
+        AJ_WarnPrintf(("AJ_HandleIntrospectRequest() NO MATCH for %s\n", msg->objPath));
         AJ_MarshalErrorMsg(msg, reply, AJ_ErrServiceUnknown);
     }
     return status;
@@ -616,7 +617,7 @@ static uint8_t CheckIndex(const void* ptr, uint8_t index, size_t stride)
     }
     do {
         if (*((void**)ptr) == NULL) {
-            AJ_Printf("\n!!!Invalid msg identifier indicates programming error!!!\n");
+            AJ_ErrPrintf(("\n!!!Invalid msg identifier indicates programming error!!!\n"));
             return FALSE;
         }
         ptr = (((uint8_t*)ptr) + stride);
@@ -848,7 +849,7 @@ AJ_Status AJ_UnmarshalPropertyArgs(AJ_Message* msg, uint32_t* propId, char* sig,
                     if (status == AJ_OK) {
                         secure = SecurityApplies(*desc, obj, objectLists[oIndex]);
                         *propId = (oIndex << 24) | (pIndex << 16) | (iIndex << 8) | mIndex;
-                        AJ_Printf("Identified property %x sig \"%s\"\n", *propId, sig);
+                        AJ_InfoPrintf(("Identified property %x sig \"%s\"\n", *propId, sig));
                     }
                     break;
                 }
@@ -861,7 +862,7 @@ AJ_Status AJ_UnmarshalPropertyArgs(AJ_Message* msg, uint32_t* propId, char* sig,
      */
     if ((status == AJ_OK) && secure && !(msg->hdr->flags & AJ_FLAG_ENCRYPTED)) {
         status = AJ_ERR_SECURITY;
-        AJ_Printf("Security violation accessing property\n");
+        AJ_WarnPrintf(("Security violation accessing property\n"));
     }
     return status;
 }
@@ -886,7 +887,7 @@ AJ_Status AJ_IdentifyMessage(AJ_Message* msg)
             status = LookupMessageId(objectLists[oIndex], msg, &secure);
             if (status == AJ_OK) {
                 msg->msgId |= (oIndex << 24);
-                AJ_Printf("Identified message %x\n", msg->msgId);
+                AJ_InfoPrintf(("Identified message %x\n", msg->msgId));
                 break;
             }
         }
@@ -901,6 +902,10 @@ AJ_Status AJ_IdentifyMessage(AJ_Message* msg)
             AJ_Message reply;
             AJ_MarshalStatusMsg(msg, &reply, status);
             status = AJ_DeliverMsg(&reply);
+            /*
+             * Cleanup the message we are ignoring.
+             */
+            AJ_CloseMsg(msg);
         }
     } else {
         ReplyContext* repCtx = FindReplyContext(msg->replySerial);
@@ -958,7 +963,7 @@ AJ_Status AJ_AllocReplyContext(AJ_Message* msg, uint32_t timeout)
             AJ_InitTimer(&repCtx->callTime);
             return AJ_OK;
         } else {
-            AJ_Printf("Failed to allocate a reply context\n");
+            AJ_ErrPrintf(("Failed to allocate a reply context\n"));
             return AJ_ERR_RESOURCES;
         }
     }

src/aj_msg.c
@@ -853,9 +853,9 @@ AJ_Status AJ_UnmarshalMsg(AJ_BusAttachment* bus, AJ_Message* msg, uint32_t timeo
         AJ_DumpMsg("RECEIVED", msg, FALSE);
     } else {
         /*
-         * TODO - should this be silent?
+         * Silently discard message unless in debug mode
          */
-        AJ_Printf("Discarding bad message %d\n", status);
+        AJ_ErrPrintf(("Discarding bad message %s\n", AJ_StatusText(status)));
         AJ_DumpMsg("DISCARDING", msg, FALSE);
         AJ_CloseMsg(msg);
     }

commit 857a2fc5c8747ec4c2ad51d6dc9f928c56fb3fd7

AllJoyn-2273: Fix delivery of partial marshaling that was broken by change e69419ec6cec98ca6bcb89347d62e1cfd55de134.

src/aj_bus.c
@@ -337,9 +337,11 @@ AJ_Status AJ_BusHandleBusMessage(AJ_Message* msg)
     AJ_Message reply;
 
     /*
-     * Set hdr to NULL so we can detect below if we have a reply to send.
+     * Check we actually have a message to handle
      */
-    reply.hdr = NULL;
+    if (!msg->hdr) {
+        return AJ_OK;
+    }
 
     switch (msg->msgId) {
     case AJ_METHOD_PING:
@@ -414,12 +416,12 @@ AJ_Status AJ_BusHandleBusMessage(AJ_Message* msg)
         break;
 
     default:
-        if (msg->hdr && (msg->hdr->msgType == AJ_MSG_METHOD_CALL)) {
+        if (msg->hdr->msgType == AJ_MSG_METHOD_CALL) {
             status = AJ_MarshalErrorMsg(msg, &reply, AJ_ErrRejected);
         }
         break;
     }
-    if ((status == AJ_OK) && reply.hdr) {
+    if ((status == AJ_OK) && (msg->hdr->msgType == AJ_MSG_METHOD_CALL)) {
         status = AJ_DeliverMsg(&reply);
     }
     return status;

src/aj_introspect.c
@@ -68,7 +68,7 @@ typedef void (*XMLWriterFunc)(void* context, const char* str, uint32_t len);
 #define PROPERTY   MEMBER_TYPE('@')  /* ((0x40 >> 4) - 2) == 2 */
 
 #define SECURE_TRUE  '$' /* Security is required for an interface that start with a '$' character */
-#define SECURE_FALSE '#' /* Security is never required for an interface that starts with a '#' character */
+#define SECURE_OFF   '#' /* Security is OFF, i.e. never required for an interface that starts with a '#' character */
 
 
 static const char* const MemberOpen[] = {
@@ -102,7 +102,7 @@ static const char nodeOpen[] = "<node";
 static const char nodeClose[] = "</node>\n";
 static const char annotateSecure[] = "  <annotation name=\"org.alljoyn.Bus.Secure\" value=\"";
 static const char secureTrue[] = "true\"/>\n";
-static const char secureNA[] = "n/a\"/>\n";
+static const char secureOff[] = "off\"/>\n";
 
 
 static char ExpandAttribute(XMLWriterFunc XMLWriter, void* context, const char** str, const char* pre, const char* post)
@@ -142,13 +142,13 @@ static AJ_Status ExpandInterfaces(XMLWriterFunc XMLWriter, void* context, const 
     while (*iface) {
         const char* const* entries = *iface;
         char flag = entries[0][0];
-        if ((flag == SECURE_TRUE) || (flag == SECURE_FALSE)) {
+        if ((flag == SECURE_TRUE) || (flag == SECURE_OFF)) {
             /*
              * if secure, skip the first char (the $) of the name
              */
             XMLWriteTag(XMLWriter, context, "<interface", nameAttr, entries[0] + 1, 0, FALSE);
             XMLWriter(context, annotateSecure, 0);
-            XMLWriter(context, (flag == SECURE_TRUE) ? secureTrue : secureNA, 0);
+            XMLWriter(context, (flag == SECURE_TRUE) ? secureTrue : secureOff, 0);
         } else {
             XMLWriteTag(XMLWriter, context, "<interface", nameAttr, entries[0], 0, FALSE);
         }
@@ -431,7 +431,7 @@ static uint32_t SecurityApplies(const char* ifc, const AJ_Object* obj, const AJ_
     if (*ifc == SECURE_TRUE) {
         return TRUE;
     }
-    if (*ifc == SECURE_FALSE) {
+    if (*ifc == SECURE_OFF) {
         return FALSE;
     }
     if (obj->flags & AJ_OBJ_FLAG_SECURE) {
@@ -555,7 +555,7 @@ static AJ_InterfaceDescription FindInterface(const AJ_InterfaceDescription* inte
                 /*
                  * Skip security specifier when comparing the interface name
                  */
-                if ((*intfName == SECURE_TRUE) || (*intfName == SECURE_FALSE)) {
+                if ((*intfName == SECURE_TRUE) || (*intfName == SECURE_OFF)) {
                     ++intfName;
                 }
                 if (strcmp(intfName, iface) == 0) {
@@ -662,7 +662,7 @@ static AJ_Status UnpackMsgId(uint32_t msgId, const char** objPath, const char** 
         /*
          * Skip over security specifier if there is one
          */
-        if ((ifc[0][0] == SECURE_TRUE) || (ifc[0][0] == SECURE_FALSE)) {
+        if ((ifc[0][0] == SECURE_TRUE) || (ifc[0][0] == SECURE_OFF)) {
             *iface = *ifc + 1;
         } else {
             *iface = *ifc;